69cf0257e47e6312c553aa1b766e0e671ad8fd21
Siddhartha Sarkar <siddsarkar7@gmail.com>
2021-03-17 12:09:55 +0530

    fixed ui responsiveness

--- src/main.js
+++ src/main.js
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { fetchNews } from './Redux/ActionCreators';
-import App from './app/App'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import App from "./app/App";
+import { fetchNews } from "./redux/ActionCreators";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     news: state.news,
-  }
-}
+  };
+};
 
-const mapDispatchToProps = dispatch => ({
-  fetchNews: (num) => dispatch(fetchNews(num))
-})
+const mapDispatchToProps = (dispatch) => ({
+  fetchNews: (num) => dispatch(fetchNews(num)),
+});
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-  }
   /*componentDidMount() {
     this.props.fetchNews();
   }*/
   render() {
     //let { data } = this.props.news.news;
 
-    return (
-      <App data={this.props.news} page={this.props.fetchNews} />
-    )
+    return <App data={this.props.news} page={this.props.fetchNews} />;
   }
 }
 
-export default (connect(mapStateToProps, mapDispatchToProps)(Main));
+export default connect(mapStateToProps, mapDispatchToProps)(Main);

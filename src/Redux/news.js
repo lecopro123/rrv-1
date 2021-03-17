6a9bb1598f68e1988d50e8147ab98ee1c588d18a
promugdha bhattacharya <promugdhabhattacharya@gmail.com>
2021-03-18 01:51:16 +0530

    final

--- src/Redux/news.js
+++ src/Redux/news.js
@@ -1,6 +1,5 @@
 import * as Action from "./ActionTypes";
 
-<<<<<<< HEAD
 export const News = (state = { isLoading: true, news: [], coll: [], error: null }, action) => {
     switch (action.type) {
         case Action.Add_news:
@@ -13,20 +12,3 @@ export const News = (state = { isLoading: true, news: [], coll: [], error: null 
         default: return state
     }
 }
-=======
-export const News = (
-  state = { isLoading: true, news: [], error: null },
-  action
-) => {
-  switch (action.type) {
-    case Action.Add_news:
-      return { ...state, isLoading: false, news: action.payload, error: null };
-    case Action.Load_news:
-      return { ...state, isLoading: true, news: [], error: null };
-    case Action.Fail_news:
-      return { ...state, isLoading: false, news: [], error: action.payload };
-    default:
-      return state;
-  }
-};
->>>>>>> 5241ac729e162713d45192397fd415696455459d
--- src/main.js
+++ src/main.js
@@ -6,14 +6,9 @@ import { fetchNews } from "./redux/ActionCreators";
 const mapStateToProps = (state) => {
   return {
     news: state.news,
-<<<<<<< HEAD
     collections: state.news.coll
   }
 }
-=======
-  };
-};
->>>>>>> 5241ac729e162713d45192397fd415696455459d
 
 const mapDispatchToProps = (dispatch) => ({
   fetchNews: (num) => dispatch(fetchNews(num)),
@@ -26,13 +21,9 @@ class Main extends Component {
   render() {
     //let { data } = this.props.news.news;
 
-<<<<<<< HEAD
     return (
       <App data={this.props.news} page={this.props.fetchNews} s={this.props.collections} />
     )
-=======
-    return <App data={this.props.news} page={this.props.fetchNews} />;
->>>>>>> 5241ac729e162713d45192397fd415696455459d
   }
 }
 
